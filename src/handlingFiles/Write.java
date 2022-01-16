0378d8f0756ece4d6042b90ca709316dc4e9ad15
RohithMolumuri <rohithkumar.molumuri@gmail.com>
2022-01-16 20:34:44 +0530

    Java FSD Phase 1 Practice Project

---
+++ src/handlingFiles/Write.java
@@ -0,0 +1,20 @@
+package handlingFiles;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Write {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+	FileWriter f=new FileWriter("C:\\\\Users\\\\rohit\\\\abc.txt");
+
+	f.write("welcome to java");
+	f.close();
+	System.out.println("succesfilly updated");
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+	}
+}
